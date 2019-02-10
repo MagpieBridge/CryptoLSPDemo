79df99c82ad923f3ea07adedd8fedd2f62b3bc7c
Julian Dolby <juliandolby@gmail.com>
2019-02-10 12:16:36 -0500

    fixes

--- src/main/java/CryptoDemoMain.java
+++ src/main/java/CryptoDemoMain.java
@@ -2,7 +2,7 @@ import magpiebridge.core.MagpieServer;
 
 public class CryptoDemoMain {
 	public static void main(String... args) {
-		String ruleDirPath = "E:/Git/Github/crypto-lsp-demo/src/test/resources";
+		String ruleDirPath = "/Users/dolby/git/CryptoLSPDemo/src/test/resources";
 		//String ruleDirPath = args[0];
 		MagpieServer server = new MagpieServer();
 		server.addAnalysis("java", new CryptoServerAnalysis(ruleDirPath));
